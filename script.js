6fee00d63f18932bbbda267693ce8018bcf774e8
aronleon <aronleonsalamon@gmail.com>
2024-10-12 19:27:43 +0200

    hg

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ async function getTabsFromLink() {
         formData.append('dt', dt);
 
         try {
-            const response = await fetch('http://192.168.0.108:5000/convertLink', {
+            const response = await fetch('https://192.168.0.108:5000/convertLink', {
                 method: 'POST',
                 body: formData
             });
